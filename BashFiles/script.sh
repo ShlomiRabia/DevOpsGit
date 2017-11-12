bde89c5accababb7e69192a347d0993997d03020
Shlomi Rabia <xoox@xoox.co.il>
2017-11-12 18:56:38 +0000

    My Bash Files2

---
+++ BashFiles/script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+MyName="$1"
+MyCount="$2"
+declare -i 
+
+#echo $MyName;
+
+
+if [[ "$MyCount" =~ [0-9]+ ]]; then 
+echo "Number: $MyCount";
+
+#mkdir $HOME/mylogs
+
+    else
+echo "";
+    echo "does no contains $?"
+
+fi
